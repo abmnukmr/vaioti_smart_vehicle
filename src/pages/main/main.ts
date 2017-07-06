61ed3ace9ba7afd7b0ce2a5ac690e429e93378c7
ArpanSamariya <arpansamria16@gmail.com>
2017-07-06 18:58:02 +0530

    text_to_speech

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import {SearchPage} from "../search/search";
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 
 /**
@@ -17,10 +18,19 @@ import {SearchPage} from "../search/search";
 })
 export class MainPage {
   bus_number:string;
-  stoppage:any=["Hamirpur","Bhota","Mehre","Badsar","Bangana","Una"];
+  stoppage:any=["Hamirpur","Sujanpur","Nadon","Anu","Una"];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl:AlertController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl:AlertController,private tts: TextToSpeech) {
     this.bus_number=this.navParams.get("bus_number");
+    this.speech();
+  }
+  speech(){
+    this.tts.speak({
+      text: 'Welcome to Live Tracking System',
+      rate: 0.8
+    })
+      .then(() => console.log('Success'))
+      .catch((reason: any) => console.log(reason));
   }
   confirm() {
     this.navCtrl.pop();
